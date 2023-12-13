102efff859dbcdf4da81e7f29153db970113ae8b
JackyChen3 <jh.chen@student.avans.nl>
2023-12-13 14:03:54 +0100

    Changed module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,9 +6,6 @@ module com.example.eindopdracht3 {
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.eindopdracht to javafx.fxml;
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     exports com.example.eindopdracht;
 }
